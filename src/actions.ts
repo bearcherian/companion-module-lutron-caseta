03fa221f16401db2a09f003d799e508096e2ac0c
Bear Cherian <bearcherian@gmail.com>
2025-12-25 23:21:32 -0600

    Refactor light functions and add fade times

--- src/actions.ts
+++ src/actions.ts
@@ -18,11 +18,11 @@ export async function UpdateActions(self: ModuleInstance): Promise<void> {
 					device.DeviceType === 'WallDimmer' ||
 					device.DeviceType === 'DivaSmartDimmer'
 				) {
-					deviceActions[`${deviceKeyName}_turn_on`] = turnOnFunction(self, areaName, device)
-					deviceActions[`${deviceKeyName}_turn_off`] = turnOffFunction(self, areaName, device)
+					deviceActions[`${deviceKeyName}_turn_on`] = createDimmerAction(self, areaName, device, 'on')
+					deviceActions[`${deviceKeyName}_turn_off`] = createDimmerAction(self, areaName, device, 'off')
 				}
 				if (device.DeviceType === 'WallDimmer' || device.DeviceType === 'DivaSmartDimmer') {
-					deviceActions[`${deviceKeyName}_set_brightness`] = setBrightnessAction(self, areaName, device)
+					deviceActions[`${deviceKeyName}_set_brightness`] = createDimmerAction(self, areaName, device, 'brightness')
 				}
 			} catch (err) {
 				self.log('error', `Error getting area for device ${device.Name}: ${(err as Error).message}`)
@@ -35,68 +35,71 @@ export async function UpdateActions(self: ModuleInstance): Promise<void> {
 	})
 }
 
-function turnOnFunction(self: ModuleInstance, areaName: string, device: DeviceDefinition): CompanionActionDefinition {
-	return {
-		name: `${areaName} ${device.Name}: Turn On `,
-		options: [],
-		callback: async () => {
-			try {
-				await self.bridge?.client.request('CreateRequest', `${device.LocalZones[0].href}/commandprocessor`, {
-					Command: { CommandType: 'GoToLevel', Parameter: [{ Type: 'Level', Value: 100 }] },
-				})
-				self.log('debug', `Turned on device: ${device.Name}`)
-			} catch (err) {
-				self.log('error', `Error turning on device ${device.Name}: ${(err as Error).message}`)
-			}
-		},
-	}
-}
-
-function turnOffFunction(self: ModuleInstance, areaName: string, device: DeviceDefinition): CompanionActionDefinition {
-	return {
-		name: `${areaName} ${device.Name}: Turn Off `,
-		options: [],
-		callback: async () => {
-			try {
-				await self.bridge?.client.request('CreateRequest', `${device.LocalZones[0].href}/commandprocessor`, {
-					Command: { CommandType: 'GoToLevel', Parameter: [{ Type: 'Level', Value: 0 }] },
-				})
-				self.log('debug', `Turned off device: ${device.Name}`)
-			} catch (err) {
-				self.log('error', `Error turning off device ${device.Name}: ${(err as Error).message}`)
-			}
-		},
-	}
-}
-
-function setBrightnessAction(
+function createDimmerAction(
 	self: ModuleInstance,
 	areaName: string,
 	device: DeviceDefinition,
+	actionType: 'on' | 'off' | 'brightness',
 ): CompanionActionDefinition {
+	const isOnOff = actionType === 'on' || actionType === 'off'
+	const fixedLevel = actionType === 'on' ? 100 : actionType === 'off' ? 0 : undefined
+
+	const options: CompanionActionDefinition['options'] = []
+
+	if (!isOnOff) {
+		options.push({
+			id: 'brightness_value',
+			type: 'number',
+			label: 'Brightness Value',
+			range: true,
+			default: 50,
+			min: 0,
+			max: 100,
+		})
+	}
+
+	options.push({
+		id: 'fade_time',
+		type: 'number',
+		label: 'Fade Time (seconds)',
+		default: 4,
+		min: 0,
+		max: 10,
+		step: 0.25,
+		range: true,
+	})
+
 	return {
-		name: `${areaName} ${device.Name}: Set Brightness`,
-		options: [
-			{
-				id: 'brightness_value',
-				type: 'number',
-				label: 'Brightness Value (0-100)',
-				default: 50,
-				min: 0,
-				max: 100,
-			},
-		],
+		name: `${areaName} ${device.Name}: ${actionType === 'on' ? 'Turn On' : actionType === 'off' ? 'Turn Off' : 'Set Brightness'}`,
+		options,
 		callback: async (event) => {
+			const level = isOnOff ? fixedLevel : (event.options.brightness_value as number)
+
+			// fade time input is in seconds but needs to be formatted for the API. So 1.75 seconds becomes "00:00:01.7500"
+			const fadeTimeValue = (event.options.fade_time as number) || 0
+			const fadeTimeFormatted = `00:00:${Math.floor(fadeTimeValue).toString().padStart(2, '0')}.${((fadeTimeValue % 1) * 10000).toFixed(0).padStart(4, '0')}`
+
 			try {
-				await self.bridge?.client.request('CreateRequest', `${device.LocalZones[0].href}/commandprocessor`, {
-					Command: {
-						CommandType: 'GoToLevel',
-						Parameter: [{ Type: 'Level', Value: event.options.brightness_value }],
+				self.log('debug', `Setting ${device.Name} to ${level}% with fade time ${fadeTimeFormatted}`)
+				const response = await self.bridge?.client.request(
+					'CreateRequest',
+					`${device.LocalZones[0].href}/commandprocessor`,
+					{
+						Command: {
+							CommandType: 'GoToDimmedLevel',
+							DimmedLevelParameters: { Level: level, FadeTime: fadeTimeFormatted },
+						},
 					},
-				})
-				self.log('debug', `Set brightness on device: ${device.Name}`)
+				)
+				if (!response?.Header.StatusCode?.code || response.Header.StatusCode.code > 299) {
+					const errorMessage = response?.Body && 'Message' in response.Body ? response.Body.Message : 'Unknown error'
+					self.log(
+						'error',
+						`Error setting ${device.Name}: ${response?.Header.StatusCode?.code} ${response?.Header.StatusCode?.message} - ${errorMessage}`,
+					)
+				}
 			} catch (err) {
-				self.log('error', `Error setting brightness on device ${device.Name}: ${(err as Error).message}`)
+				self.log('error', `Error setting ${device.Name} ${actionType || 'brightness'}: ${(err as Error).message}`)
 			}
 		},
 	}
